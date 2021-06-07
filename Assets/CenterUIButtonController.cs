7ef086020afa2ee9396bb1dac9e588efe166c06c
Ariel Marcelo Madril <ariel.m@samsung.com>
2021-06-07 13:11:06 -0400

    adding possible solution

--- Assets/CenterUIButtonController.cs
+++ Assets/CenterUIButtonController.cs
@@ -2,10 +2,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CenterUIButtonController : MonoBehaviour
 {
     private Animator m_PreviousButton;
+
+    Collider2D m_LastObject;
+
+    [SerializeField]
+    ScrollRect m_ActiveContentPanel;
+
+    [SerializeField]
+    ScrollRect m_InactiveContentPanel;
+
+    [SerializeField]
+    RectTransform m_FirstInactiveDummy;
+
+    RectTransform m_FirstActiveItem;
+
+    Coroutine m_CenterRoutine;
+
+    bool m_AllowMovement;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,20 +35,45 @@ public class CenterUIButtonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        m_InactiveContentPanel.normalizedPosition = m_ActiveContentPanel.normalizedPosition;
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if(m_LastObject != null)
+        {
+            Debug.Log("Exit " + other.name);
+            Animator lastButtonAnim = m_LastObject.GetComponent<Animator>();
+            lastButtonAnim.SetBool("IsSelected", false);
+        }
         Debug.Log("Enter " + other.name);
         Animator buttonAnim = other.GetComponent<Animator>();
         buttonAnim.SetBool("IsSelected", true);
+        m_LastObject = other;
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+    public void OnDragStarted(BaseEventData evt)
     {
-        Debug.Log("Exit " + other.name);
-        Animator buttonAnim = other.GetComponent<Animator>();
-        buttonAnim.SetBool("IsSelected", false);
+        StopCoroutine(m_CenterRoutine);
     }
+
+    public void OnDragging(BaseEventData evt)
+    {
+
+    }
+
+    public void OnDragEnded(BaseEventData evt)
+    {
+        m_CenterRoutine = StartCoroutine(CenterItem());
+    }
+
+    IEnumerator CenterItem()
+    {
+        yield return null;
+    }
+
+    /*private void OnTriggerExit2D(Collider2D other)
+    {
+    }*/
 }
